0dc3fa6c44fc342c0838244d49d2822dbdd17d9d
jaliaga_cenco <jose.aliaga@externos-ar.cencosud.com>
2022-05-28 08:26:07 -0300

    trying to add to db from models

--- entregable-clase18/MVTJoseAliaga/MVTJoseAliaga/urls.py
+++ entregable-clase18/MVTJoseAliaga/MVTJoseAliaga/urls.py
@@ -6,6 +6,6 @@ from Family.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('health/', health),
-#    path('family_index_template/',family_index),
-    path('view_family',check_family_index),
+    path('nuevo/<name>/<last_name>/<age>/',create_family_member),
+    path('view_family/',check_family_index),
 ]
