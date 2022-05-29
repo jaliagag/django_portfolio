e4de0995350b26c5a8b1e03c2c33c75da4e7431e
jaliaga_cenco <jose.aliaga@externos-ar.cencosud.com>
2022-05-29 18:59:56 -0300

    show db info

--- entregable-clase18/MVTJoseAliaga/MyFamily/views.py
+++ entregable-clase18/MVTJoseAliaga/MyFamily/views.py
@@ -4,7 +4,38 @@ from django.http import HttpResponse
 from MyFamily.models import *
 from django.template import loader
 
+import sqlite3
+
 my_template = loader.get_template('family_index.html')
+
+def dbview(table):
+    #Connecting to sqlite
+    conn = sqlite3.connect('db.sqlite3')
+    
+    #Creating a cursor object using the cursor() method
+    cursor = conn.cursor()
+    
+    a = "SELECT * FROM " + table
+    print(a)
+    #Retrieving data
+    cursor.execute(a)
+    
+    #Fetching 1st row from the table
+#    result = cursor.fetchone();
+#    print(result)
+    
+    #Fetching 1st row from the table
+    result = cursor.fetchall();
+    
+    #Commit your changes in the database
+    conn.commit()
+    
+    #Closing the connection
+    conn.close()
+
+    db = {'db': result}
+
+    return db
 
 def check_family_index(self):
 
@@ -19,7 +50,27 @@ def hi_family_member(self,name):
 
     return HttpResponse(document)
 
+def view_cousins(self):
+
+    document = my_template.render(dbview('MyFamily_cousin'))
+
+    return HttpResponse(document)
+
+def view_members(self):
+
+    document = my_template.render(dbview('MyFamily_family_member'))
+
+    return HttpResponse(document)
+
+def view_uncles(self):
+
+    document = my_template.render(dbview('MyFamily_uncle'))
+
+    return HttpResponse(document)
+
 def add_member_cousin(self,vname,vlast_name,vage,vparents,vbirth):
+    
+    dbview('MyFamily_cousin')
 
     checking = 'Primos'
 
@@ -36,6 +87,8 @@ def add_member_cousin(self,vname,vlast_name,vage,vparents,vbirth):
 
 def add_member_family_member(self,vname,vlast_name,vage,vbirth):
 
+    dbview('MyFamily_family_member')
+
     checking = 'Miembros de familia directa'
 
     mydict = {'name':vname,'last_name':vlast_name,'age':vage,'birth':vbirth,'checking':checking}
@@ -51,6 +104,8 @@ def add_member_family_member(self,vname,vlast_name,vage,vbirth):
 
 def add_member_uncle(self,vname,vlast_name,vage,vbirth):
 
+    dbview('MyFamily_uncle')
+
     checking = 'Tíos'
 
     mydict = {'name':vname,'last_name':vlast_name,'age':vage,'birth':vbirth,'checking':checking}
@@ -63,3 +118,4 @@ def add_member_uncle(self,vname,vlast_name,vage,vbirth):
     document = my_template.render(mydict)
 
     return HttpResponse(document)
+
