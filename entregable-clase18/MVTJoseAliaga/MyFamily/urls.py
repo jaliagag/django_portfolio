e4108f0ee82f32628cda8947d07d4382d7b4a341
jaliaga_cenco <jose.aliaga@externos-ar.cencosud.com>
2022-05-29 19:33:00 -0300

    add MyFamily to the path

---
+++ entregable-clase18/MVTJoseAliaga/MyFamily/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from MyFamily import views
+
+urlpatterns = [
+    path('view_family/',views.check_family_index),
+    path('members/',views.view_members),
+    path('uncles/',views.view_uncles),
+    path('cousins/',views.view_cousins),
+    path('hi/<name>/',views.hi_family_member),
+    path('add_cousin/<vname>/<vlast_name>/<vage>/<vparents>/<vbirth>/',views.add_member_cousin),
+    path('add_family_member/<vname>/<vlast_name>/<vage>/<vbirth>/',views.add_member_family_member),
+    path('add_uncle/<vname>/<vlast_name>/<vage>/<vbirth>/',views.add_member_uncle),
+]
